9a700ad74555929bffbd4c992a7152f559e35e54
Devesh Vasnani <tyndallraydv@gmail.com>
2019-03-04 18:50:02 -0500

    CreateJs dependencies added

---
+++ Scripts/game.js
@@ -0,0 +1,28 @@
+(function () {
+    // GLOBAL GAME VARIABLES
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
+    function Init() {
+        console.log("Game is Initializing");
+        Start();
+    }
+    function Start() {
+        console.log("Game is Starting");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        helloLabel.rotation += 5;
+        stage.update();
+    }
+    function Main() {
+        console.log("Game has Started");
+        helloLabel = new createjs.Text("Hello world", "40px Arial", "#000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+    }
+    window.onload = Init;
+})();
--- Scripts/game.ts
+++ Scripts/game.ts
@@ -1,4 +1,10 @@
 (function(){
+
+    // GLOBAL GAME VARIABLES
+    let canvas = document.getElementById("canvas");
+    let stage: createjs.Stage;
+    let helloLabel: createjs.Text;
+
     function Init():void{
         console.log("Game is Initializing");
         Start();
@@ -6,7 +12,25 @@
 
     function Start():void{
         console.log("Game is Starting");
+
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+
+        Main();
     }
 
+    function Update():void{
+        helloLabel.rotation += 5;
+        stage.update();
+    }
+
+    function Main():void{
+        console.log("Game has Started");
+
+        helloLabel = new createjs.Text("Hello world", "40px Arial", "#000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+    }
     window.onload = Init;
 })();
