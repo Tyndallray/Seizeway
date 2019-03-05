9ff05ef98bad6ef332bb8bea1c8fbb376c1d0be1
Devesh Vasnani <tyndallraydv@gmail.com>
2019-03-05 01:18:16 -0500

    Collision detection added

---
+++ Scripts/objects/gameobject.ts
@@ -0,0 +1,37 @@
+module objects {
+    export class GameObject extends createjs.Bitmap {
+        // Variables
+        protected speedX: number;
+        protected speedY: number;
+
+        public width:number;
+        public height:number;
+        public halfW:number;
+        public halfH:number;
+        public isColliding:boolean;
+        // Constructor
+        constructor(assetManager: createjs.LoadQueue, imageString:string) {
+            super(assetManager.getResult(imageString));
+
+            this.name = imageString;
+            this.Init();
+        }
+        // Methods / Functions
+        private Init():void {
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+            this.halfW = this.width * 0.5;
+            this.halfH = this.height * 0.5;
+
+            this.regX = this.halfW;
+            this.regY = this.halfH;
+            this.isColliding = false;
+        }
+
+        public Start():void {}
+        public Update():void {}
+        public Reset():void {}
+        public CheckBounds():void {}
+        public Move():void {}
+    }
+} 
